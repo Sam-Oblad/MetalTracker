a2803d9191c0c2fc048a1b7ba2c6ff900750b13d
Sam-Oblad <sam.oblad@gmail.com>
2024-04-27 12:41:12 -0600

    added function to interface

--- InventoryTracker/InventoryTracker.cs
+++ InventoryTracker/InventoryTracker.cs
@@ -1,34 +1,56 @@
 ﻿using Models;
 namespace InventoryTracker
 {
-    public class InventoryTracker
+    public class MetalInventoryTracker
     {
-        public HashSet<InventoryEntry> inventory;
-        public readonly float totalOunces;
-        public readonly float totalPricePaid;
+        /// <summary>
+        /// A collection of InventoryEntry objects that represent a users metal inventory
+        /// </summary>
+        public HashSet<InventoryEntry> metalInventory;
 
+        /// <summary>
+        /// Represents the total ounces within the users inventory
+        /// </summary>
+        private float totalOunces;
 
+        /// <summary>
+        /// Represents the total amount paid for the inventory
+        /// </summary>
+        private float totalPricePaid;
+
+        /// <summary>
+        /// Default constructor for MetalInventoryTracker
+        /// </summary>
+        public MetalInventoryTracker()
+        {
+            metalInventory = new HashSet<InventoryEntry>();
+            totalOunces = 0f;
+            totalPricePaid = 0f;
+        }
+
+        /// <summary>
+        /// Adds an InventoryEntry to the InventoryTracker object
+        /// </summary>
+        /// <param name="entry">The entry to be added</param>
         public void addEntry(InventoryEntry entry)
         {
-
+            metalInventory.Add(entry);
+            totalOunces += entry.ounces;
+            totalPricePaid += entry.amountPaid;
         }
 
+        /// <summary>
+        /// Removes an InventoryEntry from the InventoryTracker object
+        /// </summary>
+        /// <param name="entry">The entry to be removed</param>
         public void removeEntry(InventoryEntry entry) 
         {
-
+            if (metalInventory.TryGetValue(entry, out var e))
+            {
+                metalInventory.Remove(entry);
+                totalOunces -= entry.ounces;
+                totalPricePaid -= entry.amountPaid;
+            }
         }
-
-        private void countOunces(InventoryEntry entry) 
-        {
-
-        }
-
-        private void countTotalPricePaid(InventoryEntry entry) 
-        {
-
-        }
-
-
-
     }
 }
