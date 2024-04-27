f196b916a69efbb10faa3eb3a3baa74e586e5522
Sam-Oblad <sam.oblad@gmail.com>
2024-04-27 12:28:46 -0600

    Added documentation

--- MetalModels/InventoryEntry.cs
+++ MetalModels/InventoryEntry.cs
@@ -10,11 +10,33 @@ namespace Models
 {
     public class InventoryEntry
     {
+        /// <summary>
+        /// The date the metal was purchased
+        /// </summary>
         private string dateAcquired;
+
+        /// <summary>
+        /// The specific metal
+        /// </summary>
         private Metal metal;
+
+        /// <summary>
+        /// The amount the user paid for the metal
+        /// </summary>
         private float amountPaid;
+
+        /// <summary>
+        /// Where the metal was purchased from
+        /// </summary>
         private string location;
 
+        /// <summary>
+        /// Inventory Entry constructor
+        /// </summary>
+        /// <param name="dateAcquired">The date the metal was purchased</param>
+        /// <param name="metal">The specific metal</param>
+        /// <param name="amountPaid">The amount the user paid</param>
+        /// <param name="location">Where the metal was purchased</param>
         public InventoryEntry(string dateAcquired, Metal metal, float amountPaid, string location)
         {
             this.dateAcquired = dateAcquired;
