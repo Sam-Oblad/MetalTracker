b43d77864ddeda163111513e36f7d8a732123d00
Sam-Oblad <sam.oblad@gmail.com>
2024-04-27 12:19:29 -0600

    created inventory entry class

---
+++ MetalModels/InventoryEntry.cs
@@ -0,0 +1,26 @@
+﻿using MetalModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Models
+{
+    public class InventoryEntry
+    {
+        private string dateAcquired;
+        private Metal metal;
+        private float amountPaid;
+        private string location;
+
+        public InventoryEntry(string dateAcquired, Metal metal, float amountPaid, string location)
+        {
+            this.dateAcquired = dateAcquired;
+            this.metal = metal;
+            this.amountPaid = amountPaid;
+            this.location = location;
+        }
+    }
+}
