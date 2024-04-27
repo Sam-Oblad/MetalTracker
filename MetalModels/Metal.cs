4fa43ed6934bce07bc96ec1c933eaf8fd7f4092f
Sam-Oblad <sam.oblad@gmail.com>
2024-04-27 12:29:01 -0600

    Added documentation

--- MetalModels/Metal.cs
+++ MetalModels/Metal.cs
@@ -1,11 +1,20 @@
 ﻿namespace MetalModels
 {
+    /// <summary>
+    /// Represents a metal within an inventory
+    /// </summary>
     public abstract class Metal
     {
+        /// <summary>
+        /// Represents the market price of the metal
+        /// </summary>
         float price;
 
 
-
+        /// <summary>
+        /// Default metal constructor
+        /// </summary>
+        /// <param name="price">The price of the metal</param>
         public Metal(float price)
         {
             this.price = price;
