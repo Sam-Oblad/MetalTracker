5e6c7da23f1354526292f5e57435497d4c65347c
Sam-Oblad <sam.oblad@gmail.com>
2024-04-27 12:27:21 -0600

    Set up basic interface for InventoryTracker

--- InventoryTracker/InventoryTracker.cs
+++ InventoryTracker/InventoryTracker.cs
@@ -4,11 +4,29 @@ namespace InventoryTracker
     public class InventoryTracker
     {
         public HashSet<InventoryEntry> inventory;
+        public readonly float totalOunces;
+        public readonly float totalPricePaid;
 
 
+        public void addEntry(InventoryEntry entry)
+        {
 
+        }
 
+        public void removeEntry(InventoryEntry entry) 
+        {
 
+        }
+
+        private void countOunces(InventoryEntry entry) 
+        {
+
+        }
+
+        private void countTotalPricePaid(InventoryEntry entry) 
+        {
+
+        }
 
 
 
