977dacdf2468fe6327bfe01172c7ffd5b0660016
Sam-Oblad <99287755+Sam-Oblad@users.noreply.github.com>
2024-05-08 20:15:05 -0600

    Added basic inventory page layout

--- GUI/MainPage.xaml.cs
+++ GUI/MainPage.xaml.cs
@@ -1,12 +1,35 @@
-﻿namespace GUI
+﻿using System.Diagnostics;
+
+namespace GUI
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            InventoryPage.IsVisible = false;
+        }
+
+        private void OpenInventoryFile(Object Sender, EventArgs a)
+        {
+            ChangeToInventoryPage();
+        }
+
+        private void NewInventoryFile(Object Sender, EventArgs a)
+        {
+            ChangeToInventoryPage();
+        }
+
+        private void ChangeToInventoryPage()
+        {
+            WelcomePrompt.IsVisible = false;
+            InventoryPage.IsVisible = true;
+        }
+
+        private void NewInventoryEntry(Object Sender, EventArgs a)
+        {
+
         }
 
     }
-
 }
